ad700b62290f8a1c01be61bce7d1fa3cb21c67e5
ndevu3399 <wachiramwas122@gmail.com>
2025-03-14 09:26:56 +0300

    modify index.html and index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const form = document.getElementById("create-task-form");
+  const taskList = document.getElementById("tasks");
+
+  form.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    const taskInput = document.getElementById("new-task-description");
+    const taskText = taskInput.value.trim();
+
+    if (taskText !== "") {
+      const taskItem = document.createElement("li");
+      taskItem.textContent = taskText;
+
+      taskList.appendChild(taskItem);
+      taskInput.value = "";
+    }
+  });
 });
